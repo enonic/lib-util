c1cce888b3817be7b2088ef3f0a8badc126f6f3c
Bobby <bellfalasch@gmail.com>
2016-05-25 14:38:12 +0200

    Add function for getting json-proof app name
    This is used in many other apps and done "manually" which is a bit easy to forget. Better to just add here in lib-util to make it even more useful.

--- src/main/resources/site/lib/enonic/util/app.js
+++ src/main/resources/site/lib/enonic/util/app.js
@@ -10,3 +10,12 @@ exports.getShortName = function () {
     var appName = app.name;
     return appName.split('.').pop();
 };
+
+/**
+ * Get app's name for use in JSON-nodes (com.enonic.app as com-enonic-app)
+ * @returns {String}
+ */
+exports.getJsonName = function () {
+    var appName = app.name;
+    return appName.replace(/\./g, '-');
+};
