a4cd2aaaec5e3eafefc1eba5fe9e20def7a200b7
Christian Westgaard <cwe@enonic.com>
2016-10-25 10:56:40 +0200

    toStr - Syntactic sugar for JSON.stringify

--- src/main/resources/site/lib/enonic/util/index.js
+++ src/main/resources/site/lib/enonic/util/index.js
@@ -10,3 +10,16 @@ exports.region = require('region');
 exports.log = function (data) {
     log.info('UTIL log %s', JSON.stringify(data, null, 4));
 };
+
+/**
+ * Syntactic sugar for JSON.stringify
+ * @param {*} value
+ * @param {Function} replacer - default: null
+ * @param {String|Number} space - default: 4
+ * @returns {String} A JSON string representing the given value.
+ */
+exports.toStr = function(value) {
+    var replacer = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+    var space = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 4;
+    return JSON.stringify(value, replacer, space);
+};
