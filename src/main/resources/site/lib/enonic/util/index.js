078e5eeab6294c6258a71717d31c16766a771bd9
Christian Westgaard <cwe@enonic.com>
2016-10-31 08:58:10 +0100

    Moved value functions to value.js so they can be tested without running XP

--- src/main/resources/site/lib/enonic/util/index.js
+++ src/main/resources/site/lib/enonic/util/index.js
@@ -1,7 +1,14 @@
-exports.app = require('app');
-exports.data = require('data');
-exports.content = require('content');
-exports.region = require('region');
+exports.app = require('./app');
+exports.data = require('./data');
+exports.content = require('./content');
+exports.region = require('./region');
+
+var _value                  = require('./value');
+exports.isSet               = _value.isSet;
+exports.isNotSet            = _value.isNotSet;
+exports.valueOrEmptyString  = _value.valueOrEmptyString;
+exports.valueOr             = _value.valueOr;
+exports.ifSetPassToFunction = _value.ifSetPassToFunction;
 
 /**
  * Log data
@@ -22,55 +29,4 @@ exports.toStr = function(value) {
     var replacer = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
     var space = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 4;
     return JSON.stringify(value, replacer, space);
-};
-
-/**
- * Returns true if a value is set. Returns false if the value is NOT set.
- * @param {*} value
- * @returns true || false
- */
-exports.isSet = function (value) {
-    return value !== null && typeof value !== 'undefined';
-};
-
-/**
- * Returns true if a value is NOT set. Returns false if the value is set.
- * @param {*} value
- * @returns true || false
- */
-exports.isNotSet = function (value) {
-    return value === null || typeof value === 'undefined';
-};
-
-/**
- * Returns the value if the value is set. Otherwise returns an empty string.
- * @param {*} value
- * @returns value || ''
- */
-exports.valueOrEmptyString = function (value) {
-    return exports.isSet(value) ? value : '';
-};
-
-/**
- * Returns the value1 if the value1 is set. Otherwise returns value2.
- * @param {*} value1
- * @param {*} value2
- * @returns value || ''
- */
-exports.valueOr = function (value1, value2) {
-    return exports.isSet(value1) ? value1 : value2;
-};
-
-/**
- * If the value is set: execute the function expression with value as the first
- * and only parameter and return the return value of the function.
- * Otherwise don't even execute the function expression and return undefined.
- * @param {*} value
- * @param {function} fn
- * @returns * || undefined
- */
-exports.ifSetPassToFunction = function (value, fn) {
-    if (exports.isSet(value)) {
-        return fn(value);
-    }
 };
