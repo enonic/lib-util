78deb2834965527bcb011ebff312c3eed6bf0f81
Christian Westgaard <cwe@enonic.com>
2017-12-22 14:27:00 +0100

    isObject, isString, dlv and hasProperty

---
+++ test/object/dlv.test.js
@@ -0,0 +1,89 @@
+var _testdata = require('./testdata.js');
+var _object   = require('../../src/main/resources/site/lib/enonic/util/object.js');
+var _assert   = require('assert');
+
+var deepStrictEqual = _assert.deepStrictEqual;
+var ok              = _assert.ok;
+var throws          = _assert.throws;
+
+var dlv             = _object.dlv;
+var NOW = _testdata.NOW;
+var OBJ = _testdata.OBJ;
+
+describe('object', function() {
+  describe('dlv', function() {
+    it('undefined', function() {
+
+      throws(function() {dlv()});
+      throws(function() {dlv(undefined)});
+      throws(function() {dlv(undefined, undefined)});
+      throws(function() {dlv(undefined, undefined, undefined)});
+
+      deepStrictEqual(dlv(undefined, 'whatever'), undefined);
+      deepStrictEqual(dlv(undefined, 'whatever', 'fallback'), 'fallback');
+
+      deepStrictEqual(dlv(OBJ, 'nope'), undefined);
+      deepStrictEqual(dlv(OBJ, 'nope', 'fallback'), 'fallback');
+      deepStrictEqual(dlv(OBJ, 'nope', undefined), undefined);
+
+      deepStrictEqual(dlv(OBJ, 'keyUndefined'), undefined);
+      deepStrictEqual(dlv(OBJ, 'keyUndefined', 'fallback'), 'fallback');
+    }); // undefined
+
+    it('truthy', function() {
+      deepStrictEqual(dlv(OBJ, 'true'), true);
+      deepStrictEqual(dlv(OBJ, 'emptyObj'), {});
+      deepStrictEqual(dlv(OBJ, 'emptyArr'), []);
+      deepStrictEqual(dlv(OBJ, 'int'), 42);
+      deepStrictEqual(dlv(OBJ, 'string'), 'foo');
+      deepStrictEqual(dlv(OBJ, 'dateObj'), NOW);
+      deepStrictEqual(dlv(OBJ, 'true'), true);
+      deepStrictEqual(dlv(OBJ, 'negInt'), -42);
+      deepStrictEqual(dlv(OBJ, 'float'), 3.14);
+      deepStrictEqual(dlv(OBJ, 'negFloat'), -3.14);
+      deepStrictEqual(dlv(OBJ, 'inf'), Infinity);
+      deepStrictEqual(dlv(OBJ, 'negInf'), -Infinity);
+
+      deepStrictEqual(dlv(OBJ, 'true', 'fallback'), true);
+      deepStrictEqual(dlv(OBJ, 'emptyObj', 'fallback'), {});
+      deepStrictEqual(dlv(OBJ, 'emptyArr', 'fallback'), []);
+      deepStrictEqual(dlv(OBJ, 'int', 'fallback'), 42);
+      deepStrictEqual(dlv(OBJ, 'string', 'fallback'), 'foo');
+      deepStrictEqual(dlv(OBJ, 'dateObj', 'fallback'), NOW);
+      deepStrictEqual(dlv(OBJ, 'true', 'fallback'), true);
+      deepStrictEqual(dlv(OBJ, 'negInt', 'fallback'), -42);
+      deepStrictEqual(dlv(OBJ, 'float', 'fallback'), 3.14);
+      deepStrictEqual(dlv(OBJ, 'negFloat', 'fallback'), -3.14);
+      deepStrictEqual(dlv(OBJ, 'inf', 'fallback'), Infinity);
+      deepStrictEqual(dlv(OBJ, 'negInf', 'fallback'), -Infinity);
+    }); // thruthy
+
+    it('falsy', function() {
+      deepStrictEqual(dlv(OBJ, 'false'), false);
+      deepStrictEqual(dlv(OBJ, 'null'), null);
+      deepStrictEqual(dlv(OBJ, 'zero'), 0);
+      ok(isNaN(dlv(OBJ, 'nan')))
+      deepStrictEqual(dlv(OBJ, 'emptyString'), '');
+
+      deepStrictEqual(dlv(OBJ, 'false', 'fallback'), false);
+      deepStrictEqual(dlv(OBJ, 'null', 'fallback'), null);
+      deepStrictEqual(dlv(OBJ, 'zero', 'fallback'), 0);
+      ok(isNaN(dlv(OBJ, 'nan', 'fallback')))
+      deepStrictEqual(dlv(OBJ, 'emptyString', 'fallback'), '');
+    }); // falsy
+
+    it('Nested', function() {
+      deepStrictEqual(dlv(OBJ, 'one.two.three'), true);
+      deepStrictEqual(dlv(OBJ, 'one.two.three', 'fallback'), true);
+
+      deepStrictEqual(dlv(OBJ, 'nope.two.three'), undefined);
+      deepStrictEqual(dlv(OBJ, 'nope.two.three', 'fallback'), 'fallback');
+
+      deepStrictEqual(dlv(OBJ, 'one.nope.three'), undefined);
+      deepStrictEqual(dlv(OBJ, 'one.nope.three', 'fallback'), 'fallback');
+
+      deepStrictEqual(dlv(OBJ, 'one.two.nope'), undefined);
+      deepStrictEqual(dlv(OBJ, 'one.two.nope', 'fallback'), 'fallback');
+    });
+  }); // dlv
+}); // object
